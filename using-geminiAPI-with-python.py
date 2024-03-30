7046d22eb4fb794264904a13ccdc73c49b143f17
Zehra <130374883+alpha343434@users.noreply.github.com>
2024-03-30 22:20:37 +0300

    Create using-geminiAPI-with-python.py

---
+++ using-geminiAPI-with-python.py
@@ -0,0 +1,54 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyD4AqbtD-DvPrQ5Hnjr2Yo0hmcDSJlPtLY")
+
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+  {
+    "role": "user",
+    "parts": ["​"]
+  },
+  {
+    "role": "model",
+    "parts": ["​"]
+  },
+])
+
+convo.send_message("write a short essay about penguins")
+print(convo.last.text)
